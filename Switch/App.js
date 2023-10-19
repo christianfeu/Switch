4fdfbdb86ccea5f01451aa43c5168f80e9379b1b
Christian Feu <Chris.l.feu09@gmail.com>
2023-10-19 16:26:05 -0300

    Install react

---
+++ Switch/App.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import CadastrarUsuario from './components/Cadastrar/CadastrarUsuario';
+
+export default function App() {
+  return (
+    <CadastrarUsuario/>
+  );
+}
---
+++ Switch/components/Cadastrar/CadastrarUsuario.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, StyleSheetclear} from 'react-native';
+import { TextInput } from 'react-native-paper';
+
+export default function CadastrarUsuario(){
+    return(
+        <TouchableWithoutFeedback onPress={ () => Keyboard.dismiss() } >
+            
+        </TouchableWithoutFeedback>
+    );
+}
+
